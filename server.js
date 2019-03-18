8bb584cf04286638687244a05e89453880ceb471
Minseok Song <song.minseok@gmail.com>
2019-03-18 09:04:55 +0900

    backend completed

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const passport = require('passport')
+
+const users = require('./routes/api/users')
 
 const app = express()
 
@@ -18,6 +21,15 @@ const db = require('./config/keys').mongoURI
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB successfully connected'))
   .catch(error => console.log(error))
+
+// Passport middleware
+app.use(passport.initialize())
+
+// Passport config
+require('./config/passport')(passport)
+
+// Routes
+app.use('/api/users', users)
 
 const port = process.env.PORT || 5000
 
